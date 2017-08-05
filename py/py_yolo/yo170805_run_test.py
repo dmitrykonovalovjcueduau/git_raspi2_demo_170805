f2f6b4d09f5cccc814d7a03c03c42eb8ad9c8e7a
Dmitry Konovalov <jc138691@gmail.com>
2017-08-05 12:54:40 +1000

    ls

--- py/py_yolo/yo170805_run_test.py
+++ py/py_yolo/yo170805_run_test.py
@@ -4,11 +4,15 @@ import cv2
 # http://docs.opencv.org/trunk/d7/d8b/tutorial_py_face_detection.html
 
 def main():
+    import subprocess
     import os
     from os.path import expanduser
     home = expanduser("~")
 
+    subprocess.call("ls")
     os.chdir(os.path.join(home, 'darknet'))
+    subprocess.call("ls")
+    
     # ./darknet detector test cfg/voc.data cfg/tiny-yolo-voc.cfg tiny-yolo-voc.weights data/dog.jpg
     os.system('"./darknet detector test cfg/voc.data cfg/tiny-yolo-voc.cfg tiny-yolo-voc.weights data/dog.jpg"')
     # show_webcam()
